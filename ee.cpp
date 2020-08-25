273d0667a0a302f241be013985e9fcb6252cb0bb
snow-blade <freemanirabaruta@gmail.com>
2020-08-25 18:27:02 +0000

    Added banner and progressbar

---
+++ ee.cpp
@@ -0,0 +1,62 @@
+#include "progress.h"
+
+int main() {
+
+    int N = 2000;
+    tqdm bar;
+
+    std::cout << "Overhead of loop only:" << std::endl;
+    for(int i = 0; i < 100000000; i++) {
+        bar.progress(i, 100000000);
+    }
+    bar.finish();
+
+
+    std::cout << "Basic:" << std::endl;
+    bar.reset();
+    bar.set_theme_basic();
+    for(int i = 0; i < N; i++) {
+        bar.progress(i, N);
+        usleep(1000);
+    }
+    bar.finish();
+
+    std::cout << "Braille:" << std::endl;
+    bar.reset();
+    bar.set_theme_braille();
+    for(int i = 0; i < N; i++) {
+        bar.progress(i, N);
+        usleep(3000);
+    }
+    bar.finish();
+
+    std::cout << "Line:" << std::endl;
+    bar.reset();
+    bar.set_theme_line();
+    for(int i = 0; i < N; i++) {
+        bar.progress(i, N);
+        usleep(3000);
+    }
+    bar.finish();
+
+    std::cout << "Circles:" << std::endl;
+    bar.reset();
+    bar.set_theme_circle();
+    for(int i = 0; i < N; i++) {
+        bar.progress(i, N);
+        usleep(3000);
+    }
+    bar.finish();
+
+    bar.reset();
+    std::cout << "Vertical bars:" << std::endl;
+    bar.reset();
+    bar.set_theme_vertical();
+    for(int i = 0; i < N; i++) {
+        bar.progress(i, N);
+        usleep(3000);
+    }
+    bar.finish();
+
+    return 0;
+}
